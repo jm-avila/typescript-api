16d9e5e0ae37735bac6c390a6527f965564ca9fb
Jose Miguel Avila Gomez <josemiguelavilagomez@Joses-MacBook-Pro.local>
2021-02-02 17:30:53 +0100

    feat: account seeder

---
+++ seeders/001-account.js
@@ -0,0 +1,29 @@
+module.exports = {
+  up: async (queryInterface) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    await queryInterface.bulkInsert(
+      'account',
+      [{
+        email: "admin@mail.com",
+        password: "temp_password"
+      }],
+      { transaction },
+    );
+
+    await transaction.commit();
+  },
+  down: async (queryInterface, sequelize) => {
+    const { Op } = sequelize
+
+    const transaction = await queryInterface.sequelize.transaction();
+
+    await queryInterface.bulkDelete(
+      'account', 
+      { email: 
+        { [Op.in]: ["admin@mail.com"] } 
+      }, 
+      { transaction });
+
+    await transaction.commit();
+}
+};
